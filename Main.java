d8612d91524fe343c185f32e87c7dd48377b57b4
NyXdeity <kiritorsp123456789@gmail.com>
2024-01-11 13:10:10 +0530

    update

--- Main.java
+++ Main.java
@@ -3,9 +3,17 @@ public class Main {
       class is just names group of functions
       Main is just the name of the file
       public class has to be the same name as the name of the java file 
-
+      
     */
     public static void main(String[]args) {
         System.out.print("Hello world!");
+        /*
+          "main" this function is the entry ponit of the java program
+          "static" did'nt understand 
+          "void" to not get any value after the function returns
+           "String[]" this is an array,
+           "args" this is a argument
+           
+        */
     }
 } 
