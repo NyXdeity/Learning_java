07d511ef3579dd46338412717a9997f1de2ce47d
NyXdeity <kiritorsp123456789@gmail.com>
2024-01-11 13:56:08 +0530

    modified

--- Main.java
+++ Main.java
@@ -1,19 +1,26 @@
-public class Main { 
-    /*public means this class can e accessed from anywhere.
-      class is just names group of functions
-      Main is just the name of the file
-      public class has to be the same name as the name of the java file 
+// public class Main { 
+//     /*
+//       public means this class can be accessed from anywhere.
+//       class is just names group of functions
+//       Main is just the name of the file
+//       public class has to be the same name as the name of the java file 
       
-    */
-    public static void main(String[]args) {
-        System.out.print("Hello world!");
-        /*
-          "main" this function is the entry ponit of the java program
-          "static" did'nt understand 
-          "void" to not get any value after the function returns
-           "String[]" this is an array,
-           "args" this is a argument
-           
-        */
+//     */
+//     public static void main(String[]args) {
+//         System.out.print("Hello world!");
+//         /*
+//           "main" this function is the entry ponit of the java program
+//           "static" did'nt understand 
+//           "void" to not get any value after the function returns
+//            "String[]" this is an array,
+//            "args" this is a argument
+//         */
+//     }
+// } 
+
+// we can use different function names if we don't add the "public" before the class 
+public class Main {
+    public static void main (String[] args){
+        System.out.println("first java program");
     }
-} 
+}
