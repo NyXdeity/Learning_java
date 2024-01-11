46650ca36e9058f2d5ba809e6276fa94a37a5f48
NyXdeity <kiritorsp123456789@gmail.com>
2024-01-11 12:54:43 +0530

    update

--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
-public class Main {
+public class Main { 
+    /*public means this class can e accessed from anywhere.
+      class is just names group of functions
+      Main is just the name of the file
+      public class has to be the same name as the name of the java file 
+
+    */
     public static void main(String[]args) {
         System.out.print("Hello world!");
     }
