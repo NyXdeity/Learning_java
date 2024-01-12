216bf819bf731b8020cb4046df20c4b6d1ec8f73
NyXdeity <kiritorsp123456789@gmail.com>
2024-01-12 19:54:02 +0530

    modified

--- Main.java
+++ Main.java
@@ -22,3 +22,4 @@ public class Main {
         System.out.println("first program without any help");
     }
 }
+// i want to make a program in this file that creates an class without using the public function.
