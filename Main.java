5085828e56e235d6d9e8879c9aecd425c9ca726a
NyXdeity <kiritorsp123456789@gmail.com>
2024-01-14 22:04:37 +0530

    modified

--- Main.java
+++ Main.java
@@ -24,7 +24,6 @@
 //    }
 //}
 // I want to make a program in this file that creates a class without using the public function.
-
 //public class Main {
 //    public static void main(String[] args) {//psvm:shortcut
 //        System.out.println("first program without any help");sout: shortcut
