4648c13c6d4cca43bb48308bcb092e3e3a72e767
NyXdeity <kiritorsp123456789@gmail.com>
2024-03-24 13:06:16 +0530

    modified

--- Test.java
+++ Test.java
@@ -7,17 +7,7 @@ class Test{
         n = 11;
         m = 10;
 
-        if(n == m ){
-            System.out.println("both numbers "+n+" and "+m+" are equal");
-        }
-
-        else if (n > m){
-                System.out.println(n+" is greater than "+m);
-
-        }
-        else {
-                System.out.println(n+" is smaller than "+m);
-        }
+        System.out.println(n + " is greater than " + m);
 
     }
 }
