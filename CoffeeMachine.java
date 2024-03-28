b12aa5e4c914b3855ab102b6e7e8c50a1069b350
NyXdeity <kiritorsp123456789@gmail.com>
2024-03-28 21:07:11 +0530

    first project started

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -1,4 +1,4 @@
-public class CoffeeMachine {
+public class  CoffeeMachine {
     public static void main(String[]args){
         System.out.print("Starting to make a coffee\n" +
                 "Grinding coffee beans\n" +
--- Super.java
+++ Super.java
@@ -15,8 +15,8 @@
 //        System.out.println(exmpl);
 //    }
 //}
-public class Super {
-    public static void main(String[] args) {
+//public class Super {
+//    public static void main(String[] args) {
 
 //        int numberExample = 123;
 //
@@ -26,7 +26,57 @@ public class Super {
 //        System.out.println("Number: " + numberExample);
 //        System.out.println("Character: " + characterExample);
 //        System.out.println("String: " + stringExample);
-        String ab = "this sis ";
-        System.out.println(ab.charAt(0));
+//        String ab = "this is ";
+//        System.out.println(ab.charAt(0));//access any character of a string
+//    }
+//
+//}
+//public class Super {
+//    public static void main(String[] args) {
+//
+//       String haystack = "supercalifragilisticexpialidocious";
+//
+//        int haystackLength = 34;
+//
+//        String pattern = "cali";
+//
+//        int patternLength = 4;
+//
+//        boolean next;
+//        for (int i = 0; i < haystackLength - patternLength + 1; i++) {
+//            next = false;
+//            for (int j = 0; j < patternLength; j++) {
+//                if (haystack.charAt(i + j) != pattern.charAt(j)) {
+//                    next = true;
+//                    break;
+//                }
+//            }
+//            if (!next) {
+//                System.out.println("Pattern " + pattern + " found in " + haystack);
+//                break;
+//            }
+//        }
+//    }
+//}
+
+public class Super{
+    public static void main(String[] args) {
+
+       byte a = 10;
+
+        double b = 20.0;
+
+        char  c = 'A';
+
+        boolean d = true;
+        long e = 123456789L;
+        float f = 1.23f;
+        byte g = 127;
+        short h = 1000;
+        String result = "Integer: " + a + ", Double: " + b + ", Char: " + c +
+                ", Boolean: " + d + ", Long: " + e +
+                ", Float: " + f + ", Byte: " + g +
+                ", Short: " + h;
+        System.out.println(result);
     }
 }
